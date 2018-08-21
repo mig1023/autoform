be27d190ba9449df09d4736345c739bee4e4f408
mig1024 <mig1024@yandex.ru>
2018-08-21 20:12:12 +0300

    clean/delete fix

--- autoform_clean.pl
+++ autoform_clean.pl
@@ -12,70 +12,68 @@ use VCS::SQL;
 	log_file();
 	log_file("включение скрипта очистки autotoken");	
 	log_file("///////////////////////////////////");
+	log_file();
 
 	my $vars = new VCS::Vars(qw( VCS::Config VCS::SQL ));
 	
 	$vars->db->db_connect( $vars );
 
-	my ( $deleted, $clear ) = ( 0, 0 );
-
+	my ( $clear, $first_step ) = ( 0, 0 );
+	
+	$first_step = $vars->db->sel1("
+		SELECT count(ID) FROM AutoToken
+		WHERE LinkSended is NULL AND DATEDIFF(now(), StartDate) > 1"
+	);
+	
+	$vars->db->query("
+		DELETE FROM AutoToken
+		WHERE LinkSended is NULL AND DATEDIFF(now(), StartDate) > 1"
+	);
+	
 	my $alltokens = $vars->db->selallkeys("
-		SELECT ID, StartDate, CreatedApp, AutoAppID FROM AutoToken
-		WHERE Deleted = 0 AND DATEDIFF(now(), StartDate) > 14
-		AND ( DATEDIFF(now(), LastChange) > 3 OR LastChange is NULL )"
+		SELECT ID, StartDate, AutoAppID FROM AutoToken
+		WHERE Deleted = 0 AND
+		DATEDIFF(now(), StartDate) > 14	AND DATEDIFF(now(), LastChange) > 3"
 	);
 
 	for my $token ( @$alltokens ) {
 	
-		if ( $token->{ CreatedApp } ) {
-			
-			log_file( "чистим id " . $token->{ ID } . " --> создан " . $token->{ CreatedApp } );
-			
-			$vars->db->query("
-				UPDATE AutoToken SET Deleted = 1 WHERE ID = ?", {}, $token->{ ID }
-			);
-			
-			$clear += 1;
-		}
-		else {
-			log_file( "удаляем id " . $token->{ ID } );
-			
-			$vars->db->query("
-				DELETE FROM AutoToken WHERE ID = ?", {}, $token->{ ID }
-			);
-			
-			$deleted += 1;
-		}
+		log_file( "чистим id " . $token->{ ID } );
 		
-		if ( $token->{ AutoAppID } != 0 ) {
+		$vars->db->query("
+			UPDATE AutoToken SET Deleted = 1 WHERE ID = ?", {}, $token->{ ID }
+		);
+		
+		$clear += 1;
+		
+		next if $token->{ AutoAppID } == 0;
+		
+		$vars->db->query("
+			DELETE FROM AutoAppointments WHERE ID = ?", {}, $token->{ AutoAppID } );
+		
+		my $alldata = $vars->db->selallkeys("
+			SELECT ID, SchengenAppDataID FROM AutoAppData WHERE AppID = ?",
+			$token->{ AutoAppID }
+		);
+		
+		for my $data ( @$alldata ) {
 		
 			$vars->db->query("
-				DELETE FROM AutoAppointments WHERE ID = ?", {}, $token->{ AutoAppID } );
-			
-			my $alldata = $vars->db->selallkeys("
-				SELECT ID, SchengenAppDataID FROM AutoAppData WHERE AppID = ?",
-				$token->{ AutoAppID }
+				DELETE FROM AutoAppData WHERE ID = ?", {}, $data->{ ID } 
 			);
 			
-			for my $data ( @$alldata ) {
+			$vars->db->query("
+				DELETE FROM AutoSpbAlterAppData WHERE AppDataID = ?", {}, $data->{ ID }
+			);
 			
-				$vars->db->query("
-					DELETE FROM AutoAppData WHERE ID = ?", {}, $data->{ ID } 
-				);
-				
-				$vars->db->query("
-					DELETE FROM AutoSpbAlterAppData WHERE AppDataID = ?", {}, $data->{ ID }
-				);
-				
-				$vars->db->query("
-					DELETE FROM AutoSchengenAppData WHERE ID = ?", {}, $data->{ SchengenAppDataID }
-				);
-			}
+			$vars->db->query("
+				DELETE FROM AutoSchengenAppData WHERE ID = ?", {}, $data->{ SchengenAppDataID }
+			);
 		}
 	}
 	
 	log_file();
-	log_file( "всего удалено: $deleted очищено: $clear" );
+	log_file( "всего однодневок удалено: $first_step очищено: $clear" );
 	log_file( "скрипт завершился" );
 	
 	
