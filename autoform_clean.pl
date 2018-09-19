be8f5f4ef6991f59175b96e5263881b0d26ad06c
mig1024 <mig1024@yandex.ru>
2018-09-19 21:05:21 +0300

    autotoken del refactoring

--- autoform_clean.pl
+++ autoform_clean.pl
@@ -12,8 +12,7 @@ use VCS::SQL;
 	log_file();
 	log_file("включение скрипта очистки autotoken");	
 	log_file("///////////////////////////////////");
-	log_file();
-
+	
 	my $vars = new VCS::Vars(qw( VCS::Config VCS::SQL ));
 	
 	$vars->db->db_connect( $vars );
@@ -22,26 +21,32 @@ use VCS::SQL;
 	
 	$first_step = $vars->db->sel1("
 		SELECT count(ID) FROM AutoToken
-		WHERE LinkSended is NULL AND DATEDIFF(now(), StartDate) > 1"
+		WHERE LinkSended IS NULL AND CreatedApp IS NULL AND Finished = 0
+		AND DATEDIFF(now(), StartDate) > 1"
 	);
 	
 	$vars->db->query("
 		DELETE FROM AutoToken
-		WHERE LinkSended is NULL AND DATEDIFF(now(), StartDate) > 1"
+		WHERE LinkSended IS NULL AND CreatedApp IS NULL AND Finished = 0
+		AND DATEDIFF(now(), StartDate) > 1"
 	);
 	
 	my $alltokens = $vars->db->selallkeys("
-		SELECT ID, StartDate, AutoAppID FROM AutoToken
-		WHERE Deleted = 0 AND
-		DATEDIFF(now(), StartDate) > 14	AND DATEDIFF(now(), LastChange) > 3"
+		SELECT ID, StartDate, LastChange, EMail, LastIP, Token, AutoAppID
+		FROM AutoToken
+		WHERE CreatedApp IS NULL AND Finished = 0
+		AND DATEDIFF(now(), StartDate) > 14 AND DATEDIFF(now(), LastChange) > 3"
 	);
 
 	for my $token ( @$alltokens ) {
 	
-		log_file( "чистим id " . $token->{ ID } );
+		$vars->db->query("
+			INSERT INTO AutoToken_expired (Token, LastIP, EMail, StartDate, LastChange, RemovedDate) VALUES (?, ?, ?, ?, ?, now())", {},
+			$token->{ Token }, $token->{ LastIP }, $token->{ EMail }, $token->{ StartDate }, $token->{ LastChange }
+		);
 		
 		$vars->db->query("
-			UPDATE AutoToken SET Deleted = 1 WHERE ID = ?", {}, $token->{ ID }
+			DELETE FROM AutoToken WHERE ID = ?", {}, $token->{ ID }
 		);
 		
 		$clear += 1;
@@ -49,7 +54,8 @@ use VCS::SQL;
 		next if $token->{ AutoAppID } == 0;
 		
 		$vars->db->query("
-			DELETE FROM AutoAppointments WHERE ID = ?", {}, $token->{ AutoAppID } );
+			DELETE FROM AutoAppointments WHERE ID = ?", {}, $token->{ AutoAppID }
+		);
 		
 		my $alldata = $vars->db->selallkeys("
 			SELECT ID, SchengenAppDataID FROM AutoAppData WHERE AppID = ?",
@@ -72,7 +78,6 @@ use VCS::SQL;
 		}
 	}
 	
-	log_file();
 	log_file( "всего однодневок удалено: $first_step очищено: $clear" );
 	log_file( "скрипт завершился" );
 	
