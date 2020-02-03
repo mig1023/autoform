6fabe1832944a80b730a29662fc2c5294045a6f0
mig1023 <mig1023@yandex.ru>
2020-02-03 21:45:27 +0300

    appdata checks chng #2 (spb)

--- autodata_type_c_spb.pm
+++ autodata_type_c_spb.pm
@@ -1034,6 +1034,12 @@ sub get_content_rules_hash
 						offset => ( 10 * 365 ), # <--- 10 years
 						error => 'Дата выдачи паспорта',
 					},
+					{
+						condition => 'now_or_earlier',
+						offset => 180,
+						equality_is_also_fail => 1,
+						full_error => 'Действует ограничение на максимальную дату вылета: не более [offset] с текущей даты',
+					},
 				],
 				db => {
 					table => 'AppData',
@@ -1080,23 +1086,6 @@ sub get_content_rules_hash
 				minimal_date => 'apps_date',
 			},
 			{
-				type => 'input',
-				name => 'calcdur',
-				label => 'Продолжительность пребывания',
-				comment => {
-					'1,31,2,3,4,5,6,7,8,9,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,39,40,42,44,45' =>
-						'При запросе многократной визы на год необходимо указать 180 дней, при запросе визы на 2 года — 360 дней',
-					'27,33,38,11,30,34,29,43,37' =>
-						'Если Вы запрашиваете визу на год, укажите 180, если на два, то 180+180, на три - 180+180+180',
-				},
-				example => '14',
-				check => 'zN\+',
-				db => {
-					table => 'AppData',
-					name => 'CalcDuration',
-				},
-			},
-			{
 				type => 'free_line',
 			},
 			{
@@ -1104,23 +1093,6 @@ sub get_content_rules_hash
 				name => 'permi_text',
 				label => 'Предыдущие шенгенские визы',
 				font => 'bold',
-			},
-			{
-				type => 'free_line',
-			},
-			{
-				type => 'radiolist',
-				name => 'prevvisa',
-				label => 'Были ли визы за последние три года',
-				check => 'zN',
-				db => {
-					table => 'AppData',
-					name => 'PrevVisa',
-				},
-				param => { 
-					1 => 'нет', 
-					2 => 'да',
-				},
 			},
 			{
 				type => 'free_line',
@@ -2643,6 +2615,12 @@ sub get_content_edit_rules_hash
 					offset => ( 10 * 365 ), # <--- 10 years
 					error => 'Дата выдачи паспорта',
 				},
+				{
+					condition => 'now_or_earlier',
+					offset => 180,
+					equality_is_also_fail => 1,
+					full_error => 'Действует ограничение на максимальную дату вылета: не более [offset] с текущей даты',
+				},
 			],
 			db => {
 				table => 'AppData',
@@ -2679,18 +2657,6 @@ sub get_content_edit_rules_hash
 			},
 			special => 'datepicker, mask',
 			minimal_date => 'apps_date',
-		},
-		{
-			type => 'input',
-			name => 'edt_calcdur',
-			label => 'Продолжительность пребывания',
-			comment => 'Если Вы запрашиваете визу на год, укажите 180, если на два, то 180+180, на три - 180+180+180',
-			example => '14',
-			check => 'zWN\s\+',
-			db => {
-				table => 'AppData',
-				name => 'CalcDuration',
-			},
 		},
 		{
 			type => 'free_line',
